6d8105c45eb9a44fac7556a4a204146382b7172f
Dom Rodriguez <shymega@shymega.org.uk>
2016-08-06 14:48:32 +0100

    Ignore Assembly output

---
+++ assembly/exit.asm
@@ -0,0 +1,22 @@
+	# Setup the DATA section
+	# We evidently don't use this.
+	.section .data
+	# Setup the TEXT section
+	# We evidently don't use this.
+	.section .text
+	# Make the _start section global.
+	.globl _start
+
+# Define the _start section.
+_start:
+
+	# Move the integer '1' into the %eax register
+	# This prepares for the Linux system call
+	movl $1, %eax
+
+	# Move the integer '4' into the %ebx register
+	# This is the exit code for the program.
+	movl $4, %ebx
+
+	#Interrupt the program, and give Linux the control.
+	int $0x80
