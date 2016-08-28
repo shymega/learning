fa0fa2ada4dbff1009969f0547f2abf119592aa5
Dom Rodriguez <shymega@shymega.org.uk>
2016-08-28 21:54:57 +0100

    Add NNTP test code

---
+++ rust/nntp-test/src/main.rs
@@ -0,0 +1,33 @@
+extern crate nntp;
+
+use nntp::{Article, NNTPStream};
+
+fn main() {
+    let mut nntp_stream = match NNTPStream::connect("news.gmane.org", 119) {
+        Ok(stream) => stream,
+        Err(e) => panic!("{}", e),
+    };
+
+
+    match nntp_stream.capabilities() {
+        Ok(lines) => {
+            for line in lines.iter() {
+                println!("{}", line);
+            }
+        }
+        Err(e) => panic!(e),
+    };
+
+    match nntp_stream.group("gmane.emacs.devel") {
+        Ok(_) => {
+            println!("Group found!");
+        }
+        Err(e) => {
+            println!("ERR: Group not found.");
+            println!("ERR: :: {}", e);
+        }
+    };
+
+
+    let _ = nntp_stream.quit();
+}
