54af1474a26c5289abd7c774daaed293375940c0
Dom Rodriguez <shymega@shymega.org.uk>
2016-10-18 15:14:45 +0100

    Make exit.asm exit with exit code, nothing else

--- assembly/exit.asm
+++ assembly/exit.asm
@@ -2,17 +2,7 @@ global _start
 
 section .text
 _start:
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, msg
-    mov edx, msg.len
-    int 0x80
 
     mov eax, 1
     mov ebx, 0
-    int 0x80
-
-section .data
-msg: db  "Hello, world!", 10
-.len: equ $ - msg
-    
+    int 0x80    
