3ba0c40360a7e5e5414c66de75cfd0d437f92539
Dom Rodriguez <shymega@shymega.org.uk>
2016-10-18 15:14:39 +0100

    simplify hello-world.asm

--- assembly/hello-world.asm
+++ assembly/hello-world.asm
@@ -1,15 +1,13 @@
 section .data
     msg db "Hello, world!"
-
+    
 section .text
 global _start
-
 _start:
     mov rax, 1
     mov rdi, 1
     mov rsi, msg
     mov rdx, 13
-
     syscall
 
     mov rax, 60
