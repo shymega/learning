37ca8a72ceea83293cba9774cfbcd1c8892fa1c6
Dom Rodriguez <shymega@shymega.org.uk>
2016-10-18 13:13:26 +0100

    Add NASM Exit program

---
+++ assembly/exit.asm
@@ -0,0 +1,18 @@
+global _start
+
+section .text
+_start:
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, msg
+    mov edx, msg.len
+    int 0x80
+
+    mov eax, 1
+    mov ebx, 0
+    int 0x80
+
+section .data
+msg: db  "Hello, world!", 10
+.len: equ $ - msg
+    
