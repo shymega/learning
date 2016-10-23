54c6b516d826f1c1d20d34ab6f634b7765922515
Dom Rodriguez <shymega@shymega.org.uk>
2016-10-23 15:42:10 +0100

    Add elixir portal code

---
+++ erlang/elixir/portal/lib/portal.ex
@@ -0,0 +1,41 @@
+defmodule Portal do
+  use Application
+
+  # See http://elixir-lang.org/docs/stable/elixir/Application.html
+  # for more information on OTP Applications
+  def start(_type, _args) do
+    import Supervisor.Spec, warn: false
+
+    # Define workers and child supervisors to be supervised
+    children = [
+      # Starts a worker by calling: Portal.Worker.start_link(arg1, arg2, arg3)
+      # worker(Portal.Worker, [arg1, arg2, arg3]),
+    ]
+
+    # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Portal.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+
+  defstruct [:left, :right]
+
+
+  def transfer(left, right, data) do
+    for item <- data do
+      Portal.Door.push(left, item)
+    end
+
+    %Portal{left: left, right: right}
+  end
+
+  def push_right(portal) do
+    case Portal.Door.pop(portal.left) do
+      :error -> :ok
+      {:ok, h} -> Portal.Door.push(portal.right, h)
+    end
+
+    portal
+  end
+   
+end
