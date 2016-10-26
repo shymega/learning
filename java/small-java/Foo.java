bed605172d434f2ef9f4542af1dfdf43b3e6b2c4
Dom Rodriguez <shymega@shymega.org.uk>
2016-10-26 20:06:40 +0100

    Add Foo

---
+++ java/small-java/Foo.java
@@ -0,0 +1,15 @@
+public class Foo {
+  public static void main(final String[] args) {
+    System.out.println("This is a simple.");
+    a();
+    b();
+  }
+
+  public static void a() {
+    System.out.println("Calling function A..");
+  }
+
+  public static void b() {
+    System.out.println("Calling function B..");
+  }
+}
