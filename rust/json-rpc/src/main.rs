74ca3b66beee8d2a10f2fd3b680c79a9905c4a8d
Dom Rodriguez <shymega@shymega.org.uk>
2016-10-31 09:09:29 +0000

    json-rpc: add serialisation

--- rust/json-rpc/src/main.rs
+++ rust/json-rpc/src/main.rs
@@ -7,10 +7,17 @@ extern crate serde_json;
 extern crate jsonrpc_core;
 
 #[derive(Serialize, Deserialize, Debug)]
-struct Hello {
+struct Hello_Request {
     jsonrpc: String,
     method: String,
     params: Vec<i32>,
+    id: usize,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Hello_Response<'a> {
+    jsonrpc: String,
+    result: String,
     id: usize,
 }
 
@@ -27,7 +34,7 @@ fn main() {
     let io = IoHandler::new();
     io.add_async_method("say_hello", SayHello);
 
-    let request_struct = Hello {
+    let request_struct = Hello_Request {
         jsonrpc: "2.0".to_string(),
         method: "say_hello".to_string(),
         params: vec![42, 23],
@@ -37,13 +44,17 @@ fn main() {
     let request_json = serde_json::to_string(&request_struct).unwrap();
     let request: &str = request_json.as_str();
 
-    println!("serialized: {}", request_json);
+    //    let response =
+    //    r#"{"jsonrpc":"2.0","result":"hello","id":1}"#;
 
-    let request_dese: Hello = serde_json::from_str(&request_json).unwrap();
+    let response_struct = Hello_Response {
+        jsonrpc: "2.0".to_string(),
+        result: "hello".to_string(),
+        id: 1,
+    };
 
-    println!("deserialized: {:?}", request_dese);
-
-    let response = r#"{"jsonrpc":"2.0","result":"hello","id":1}"#;
+    let response_json = serde_json::to_string(&response_struct).unwrap();
+    let response: &str = response_json.as_str();
 
     io.handle_request(request).map(|async_response| {
         async_response.on_result(move |res| {
